d3366a9e4c8aceed1728dfd6eb4b56a9ce5eb659
Canals233 <1552043941@qq.com>
2024-04-15 23:05:55 +0800

    fix:format change

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import Tdesign from 'tdesign-vue-next'
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Tdesign)
 
 app.mount('#app')
